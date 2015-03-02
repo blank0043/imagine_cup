b292cb0af25cfbf5e706427ff690703c752ae21c
Bryan Cam <bryanrcam@gmail.com>
2015-03-02 00:03:46 -0500

    New website details

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+    <title>Paul van Oijen | Visual Designer</title>
+    <meta name="description" content="The online portfolio of Paul van Oijen; a visual designer based in Maastricht">
+    <meta name="author" content="Paul van Oijen">
+	
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/android-chrome-manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat|Varela+Round' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/style.min.css" rel="stylesheet">
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <script src="js/wow.min.js"></script>
+      <script>
+        new WOW().init();
+      </script>
+      
+      <script src="js/pace.js"></script>  
+      <link href="css/pace-theme-flash.min.css" rel="stylesheet" /> 
+      
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-35086500-2', 'auto');
+        ga('send', 'pageview');
+
+      </script>
+  </head>
+  
+  <body>
+    <div class="container-push">
+      <div class="menu-wrap">
+        <nav class="menu">
+          <div class="icon-list">
+            <a href="index.php"><img class="hidden-xs" src="img/logo2.png" alt="Logo for Paul van Oijen"/></a>
+            <a id="active" href="index.php"><i class="fa fa-home icon-margin"></i><span>Home</span></a>
+            <a href="about.php"><i class="fa fa-user icon-margin"></i><span>About</span></a>
+            <a href="portfolio.php"><i class="fa fa-picture-o"></i><span>Portfolio</span></a>
+            <a href="resume.php"><i class="fa fa-list"></i><span>Resume</span></a>
+            <a href="contact.php"><i class="fa fa-envelope"></i><span>Contact</span></a>
+          </div>
+        </nav>
+        <button class="close-button" id="close-button">Close Menu</button>
+      </div>
+      <button class="menu-button animated fadeIn" id="open-button">Open Menu</button>
+      <!-- Start page content -->
+      <div class="content-wrap">
+        <div class="content">
+          <header class="container-fluid intro-lg uppercase bkg">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
+              <h3 id="supra" class="animated fadeInUp">The online portfolio of</h3>
+              <h1 id="title" class="animated fadeInUp">Paul van Oijen</h1>
+              <h3 id="sub" class="animated bounceIn">A visual designer <span class="hidden-xs">from Maastricht</span><span class="hidden-xs hidden-sm hidden-md">, the Netherlands.</span></h3>
+               <div class="divider divider-intro animated bounceIn"></div>
+              <a id="btn-intro" href="contact.php" class="btn btn-custom animated fadeInUp">Get in touch</a>
+            </div>
+          </header>
+          <div class="expertise">
+            <div class="row">
+              <div class="col-lg-12">
+                <h2 class="uppercase">Services</h2>
+                <div class="divider"></div>	
+              </div>
+            </div>
+            <div class="container expertise">
+                <div class="col-xs-10 col-xs-offset-2 col-md-10 col-centered">
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-4 wow fadeIn">
+                      <img src="https://mail.google.com/mail/u/0/?ui=2&ik=02d1ad6dac&view=fimg&th=14bcd689fd1246cf&attid=0.8&disp=inline&safe=1&attbid=ANGjdJ_ZFcB65RrNO0WqEc6a_rDVbA_3FapuHzSOMvisXmzq482a1YPQ4F8w5l7_tULCBS_2rmQFd9aNqmzyEK70aIhCg3zKp3ZfJ1lXgCcz7El1TV0QutKSpd3dZFc&ats=1425081747850&rm=14bcd689fd1246cf&zw&sz=w1890-h842" alt="Graphic Design">
+                      <h4 class="uppercase">What is it?</h4>
+                      <div class="divider-small"></div>
+                      <p>Company branding, business cards, stationery and everything else you may need to kick-start your website.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4 wow fadeIn">
+                      <img src="https://mail.google.com/mail/u/0/?ui=2&ik=02d1ad6dac&view=fimg&th=14bcd689fd1246cf&attid=0.8&disp=inline&safe=1&attbid=ANGjdJ9H6HMQqpTr38OtyZF5Vo1adizruR2rXHvfWdqyNMSurznVLaT8ucB7PKDbw6fwezIGGVHQp_t22Iw_Lk1qgrjS8lgCJjrhHHH57RBQpVmmaI2b7TQT1vALqs8&ats=1425082906632&rm=14bcd689fd1246cf&zw&sz=w1890-h842" alt="Web Design">
+                      <h4 class="uppercase">How to Use it?</h4>
+                      <div class="divider-small"></div>
+                      <p>From basic wireframes through to full-fledged designs that reflect you or your brand; I'm your go-to guy.</p>       
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4 wow fadeIn">
+                      <img src="https://mail.google.com/mail/u/0/?ui=2&ik=02d1ad6dac&view=fimg&th=14bcd689fd1246cf&attid=0.8&disp=inline&safe=1&attbid=ANGjdJ9H6HMQqpTr38OtyZF5Vo1adizruR2rXHvfWdqyNMSurznVLaT8ucB7PKDbw6fwezIGGVHQp_t22Iw_Lk1qgrjS8lgCJjrhHHH57RBQpVmmaI2b7TQT1vALqs8&ats=1425082906632&rm=14bcd689fd1246cf&zw&sz=w1890-h842" alt="Web Development">
+                      <h4 class="uppercase">Why Glucometer?</h4>
+                      <div class="divider-small"></div>
+                      <p>Responsive, easy to use sites are my forte. From small static sites to multiple-page, complex beasts.</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+            <div class="container-fluid recent-work">
+              <div class="col xs-12 col-md-8 col-md-offset-2">
+                <h3 class="text-center uppercase">Recent work</h3>
+                <div class="divider"></div>
+                <div class="row">
+                  <div class="col-xs-12 col-md-6">
+                    <div class="grid wow fadeInLeftSmall">
+                      <a href="paul.php">
+                        <figure class="grid-item">
+                          <img src="https://mail.google.com/mail/u/0/?ui=2&ik=02d1ad6dac&view=fimg&th=14bcd689fd1246cf&attid=0.4&disp=inline&safe=1&attbid=ANGjdJ_BuAURYXueXRyAP9tZBqS7IopIlUK57k0J4Pym1ZdGAl0AgfVUYV3oQ8x--k8aNTkrj-dlPKUlsWr7Op3TDP2774X2QjSwkUFjyBTlu0WdBV0OBk0fJl__-Bc&ats=1425091758222&rm=14bcd689fd1246cf&zw&sz=w1890-h842" alt="Personal Brand"/>
+                          <figcaption>
+                            <h2>Personal Brand</h2>
+                            <p>Paul van Oijen.com</p>
+                          </figcaption>			
+                        </figure>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xs-12 col-md-6">
+                    <div class="grid wow fadeInRightSmall">
+                      <a href="superhip.php">
+                        <figure class="grid-item">
+                          <img src="https://mail.google.com/mail/u/0/?ui=2&ik=02d1ad6dac&view=fimg&th=14bcd689fd1246cf&attid=0.7&disp=inline&safe=1&attbid=ANGjdJ8aijHde-PARMzdJQlZ6QZL4Fjtvh9qM2reubm1DtTt0FHbYWZmI0V250hcPzdlfssblhXuowZQYv4j8Q16FiPbdVBDYUJzmamseToNkBjI63hOdEzjR-QhwY4&ats=1425091758223&rm=14bcd689fd1246cf&zw&sz=w1890-h842" alt="Superhip" />
+                          <figcaption>
+                            <h2>SuperHip</h2>
+                              <p>Concept store</p>
+                            </figcaption>			
+                        </figure>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xs-12 text-center">
+                    <a href="portfolio.php" class="btn btn-custom">See all projects <span class="fa fa-chevron-right"></span></a>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <!-- Footer -->
+        <footer>
+          <ul class="list-inline social">
+          <li><a href="https://twitter.com/paulvanoijen" target="_blank" class="social-twitter"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></i></a></li>
+            <li><a href="https://github.com/paulvano" target="_blank" class="social-git"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-github-alt fa-stack-1x fa-inverse"></i></i></a></li>
+          <li><a href="https://www.behance.net/paulvanoijen" target="_blank" class="social-behance"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-behance fa-stack-1x fa-inverse"></i></i></a></li>
+          <li><a href="mailto:info@paulvanoijen.com" class="social-mail"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></i></a></li>
+        </ul>
+            <p>Made with <span class="fa fa-heart animated pulse"></span> in Maastricht.</p>
+          <p>&copy; 2014 Paul van Oijen. All rights reserved.</p>
+        </footer>	  
+      </div><!-- !.wrap -->
+    </div><!-- !.container-push -->
+	
+    <!-- Javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
